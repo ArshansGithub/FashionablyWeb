8902f23afd130bf18351a9b6110e483f8ce0b4a6
Arshan <arshan@arshan.dev>
2024-05-18 12:09:30 -0700

    Beginning
    Co-authored-by: Nathan Gillespie <crookedtwig4@gmail.com>

--- README.md
+++
@@ -1,2 +0,0 @@
-# Triton2024Web
-React + Next.JS + Tailwind CSS + Typescript
---
+++ tritonhacks2024/app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        
+      </div>
+    </main>
+  );
+}
+
+/*
+Fast fashion:
+fast degrading _____
+cite all sources (prompt?)
+fashionably
+"the proper way to fashion"
+"sustainable, clean"
+"sustainable fashion brought to you"
+"sf" = "sustainable fashion"
+"sf a click away"
+"Fashionably = name"
+placeholder api
+loading animation
+1 page
+dynamic loading & deloading
+modules
+
+*/
